ec9913a4a1199bfe055506c91d7f86671c1fdaeb
zheer barzan <zheerbarzan85@outlook.com>
2024-12-23 20:08:20 +0300

    Initial Commit

---
+++ medusa/medusaApp.swift
@@ -0,0 +1,17 @@
+//
+//  medusaApp.swift
+//  medusa
+//
+//  Created by Zheer Barzan on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct medusaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
